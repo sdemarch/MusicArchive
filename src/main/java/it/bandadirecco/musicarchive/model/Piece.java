230256ce6b08e9d337a17bd3e87be614a5eff2c5
iagosd <stefano_demarchi@outlook.com>
2020-06-22 10:15:04 +0200

    Update Piece entity

--- src/main/java/it/bandadirecco/musicarchive/model/Piece.java
+++ src/main/java/it/bandadirecco/musicarchive/model/Piece.java
@@ -29,6 +29,8 @@ public class Piece {
     @OneToOne
     private Publisher publisher;
 
+    private String notes;
+
     public Piece() {
         this.title = "No title";
         this.folderNumber = 0;
@@ -37,12 +39,13 @@ public class Piece {
         this.publisher = new Publisher();
     }
 
-    public Piece(String title, long folderNumber, Author composer, Author arranger, Publisher publisher) {
+    public Piece(String title, long folderNumber, Author composer, Author arranger, Publisher publisher, String notes) {
         this.title = title;
         this.folderNumber = folderNumber;
         this.composer = composer;
         this.arranger = arranger;
         this.publisher = publisher;
+        this.notes = notes;
     }
 
     public long getId() {
@@ -87,5 +90,26 @@ public class Piece {
 
     public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Piece {" +
+                "id = " + id +
+                ", title = '" + title + "'" +
+                ", folderNumber = " + title +
+                ", composer = " + composer.toString() +
+                ", arranger = " + arranger.toString() +
+                ", publisher = " + publisher.toString() +
+                ", notes = '" + notes + "'" +
+                "}";
     }
 }
